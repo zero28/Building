1fc8db2f5782421c7da4d1af758b41370ce7d393
zero28 <junxiongzhou@junxiongzhou-macbookpro.roam.corp.google.com>
2015-11-09 17:52:17 +0800

    Add command

--- src/com/gmail/zhou1992228/building/Building.java
+++ src/com/gmail/zhou1992228/building/Building.java
@@ -1,16 +1,17 @@
 package com.gmail.zhou1992228.building;
 
-import net.milkbowl.vault.economy.Economy;
-
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gmail.zhou1992228.building.command.CommandAddBuilding;
 import com.gmail.zhou1992228.building.command.CommandCollect;
+import com.gmail.zhou1992228.building.command.CommandPut;
 import com.gmail.zhou1992228.building.task.TaskDamageBuildings;
 import com.gmail.zhou1992228.building.task.TaskUpdateBuilding;
 import com.gmail.zhou1992228.building.task.TaskValidateBuilding;
+
+import net.milkbowl.vault.economy.Economy;
 
 public class Building extends JavaPlugin {
 
@@ -37,6 +38,7 @@ public class Building extends JavaPlugin {
 		ins = this;
 		this.getCommand("ttbd").setExecutor(new CommandAddBuilding());
 		this.getCommand("collect").setExecutor(new CommandCollect());
+		this.getCommand("put").setExecutor(new CommandPut());
 		this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TaskUpdateBuilding(), 5 * 20, 5 * 20);
 		this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TaskValidateBuilding(), 10 * 20, 10 * 20);
 		this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TaskDamageBuildings(), 10 * 20, 10 * 20);
