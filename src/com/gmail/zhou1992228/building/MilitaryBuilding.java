8d5507797f690155f49170eae1e93fad2d42a280
zero28 <junxiongzhou@junxiongzhou-macbookpro.roam.corp.google.com>
2015-11-10 19:08:47 +0800

    military

--- src/com/gmail/zhou1992228/building/MilitaryBuilding.java
+++ src/com/gmail/zhou1992228/building/MilitaryBuilding.java
@@ -1,14 +1,20 @@
 package com.gmail.zhou1992228.building;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
+import com.gmail.zhou1992228.building.util.Util;
+
 public class MilitaryBuilding extends BuildingEntity {
 
 	public MilitaryBuilding(ConfigurationSection config) {
 		super(config);
+		rewards = config.getStringList("reward_list");
 	}
 
 	public MilitaryBuilding(String owner, Location pos, String type, String name) {
@@ -20,16 +26,34 @@ public class MilitaryBuilding extends BuildingEntity {
 	}
 	
 	@Override
-	public void onCollect(Player p, int count) {
-		// TODO
+	public void Save(ConfigurationSection config) {
+		super.Save(config);
+		config.set("reward_list", rewards);
 	}
+	
+	@Override
+	public void onCollect(Player p, int count) {
+		if (rewards.size() == 0) {
+			p.sendMessage("并没有战利品");
+			return;
+		}
+		Util.giveItems(p, rewards.get(0));
+		rewards.remove(0);
+	}
+	
 	@Override
 	public void onDamage(Entity entity) {
 		// TODO
+	}
+	
+	public void addResource(String s) {
+		rewards.add(s);
 	}
 	
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
 	}
+	
+	private List<String> rewards = new ArrayList<String>();
 }
